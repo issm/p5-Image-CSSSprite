a5588a5229d2f44f1b0ce39f1a287f43f6ef129b
issm <issmxx@gmail.com>
2010-12-12 09:02:26 +0900

    initial commit

---
+++ lib/Image/CSSSprite.pm
@@ -0,0 +1,32 @@
+package Image::CSSSprite;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Image::CSSSprite -
+
+=head1 SYNOPSIS
+
+  use Image::CSSSprite;
+
+=head1 DESCRIPTION
+
+Image::CSSSprite is
+
+=head1 AUTHOR
+
+issm E<lt>issmxx@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
